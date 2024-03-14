2f61d0997eafc305543340e139e8e1557eea8592
MathNerd28 <xander.bhalla@gmail.com>
2024-03-14 17:54:07 -0400

    Add base Endpoint with auth

---
+++ frc-events-api-java/src/main/java/org/victorrobotics/frcevents/Endpoint.java
@@ -0,0 +1,162 @@
+package org.victorrobotics.frcevents;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+
+import okhttp3.CacheControl;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public final class Endpoint<T> implements Supplier<Optional<T>> {
+  private static final Map<String, WeakReference<Endpoint<?>>> ENDPOINTS = new HashMap<>();
+
+  private static final String API_URL = "https://frc-api.firstinspires.org/v3.0";
+  private static final String AUTH_HEADER;
+
+  static {
+    String username = System.getenv("FRC_EVENTS_API_USERNAME");
+    String token = System.getenv("FRC_EVENTS_API_TOKEN");
+    String authStr = username + ":" + token;
+    AUTH_HEADER = "Basic " + Base64.getEncoder()
+                                   .encodeToString(authStr.getBytes(StandardCharsets.UTF_8));
+  }
+
+  private static final OkHttpClient HTTP_CLIENT = new OkHttpClient();
+
+  static final ObjectMapper JSON_OBJECT_MAPPER =
+      new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                        .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+                        .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+
+  private static ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+
+  private final Request.Builder requestBuilder;
+  private final ObjectReader    jsonReader;
+
+  @SuppressWarnings("java:S3077")
+  private volatile Optional<T> value;
+
+  private Endpoint(String endpoint, ObjectReader reader) {
+    jsonReader = reader;
+    requestBuilder = new Request.Builder().get()
+                                          .url(API_URL + endpoint)
+                                          .header("Authorization", AUTH_HEADER)
+                                          .cacheControl(CacheControl.FORCE_NETWORK);
+    value = Optional.empty();
+  }
+
+  public Optional<T> get() {
+    return value;
+  }
+
+  public Optional<T> refresh() {
+    try {
+      Call call = HTTP_CLIENT.newCall(requestBuilder.build());
+      Response response = call.execute();
+      update(response);
+    } catch (IOException e) {}
+
+    return get();
+  }
+
+  public CompletableFuture<Optional<T>> refreshAsync() {
+    return CompletableFuture.supplyAsync(requestBuilder::build, EXECUTOR)
+                            .thenApply(HTTP_CLIENT::newCall)
+                            .thenApply(call -> {
+                              try {
+                                return call.execute();
+                              } catch (IOException e) {
+                                throw new CompletionException(e);
+                              }
+                            })
+                            .thenAccept(this::update)
+                            .thenApply(x -> get());
+  }
+
+  private void update(Response response) {
+    try (response) {
+      int statusCode = response.code();
+      if (statusCode != 200 && statusCode != 304) return;
+
+      String etag = response.headers()
+                            .get("etag");
+      if (etag != null) {
+        requestBuilder.header("If-None-Match", etag);
+      }
+
+      if (statusCode != 200) return;
+
+      try (InputStream body = response.body()
+                                      .byteStream()) {
+        T result = jsonReader.readValue(body);
+        if (value.isEmpty() || !value.get()
+                                     .equals(result)) {
+          value = Optional.of(result);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  static <T> Endpoint<T> forSingle(String endpoint, Class<T> clazz) {
+    WeakReference<Endpoint<?>> value = ENDPOINTS.get(endpoint);
+    if (value == null || value.get() == null) {
+      value = new WeakReference<>(new Endpoint<>(endpoint, JSON_OBJECT_MAPPER.readerFor(clazz)));
+      ENDPOINTS.put(endpoint, value);
+    }
+    return (Endpoint<T>) value.get();
+  }
+
+  @SuppressWarnings("unchecked")
+  static <T> Endpoint<List<T>> forList(String endpoint, Class<T> clazz) {
+    WeakReference<Endpoint<?>> value = ENDPOINTS.get(endpoint);
+    if (value == null || value.get() == null) {
+      value =
+          new WeakReference<>(new Endpoint<>(endpoint, JSON_OBJECT_MAPPER.readerForListOf(clazz)));
+      ENDPOINTS.put(endpoint, value);
+    }
+    return (Endpoint<List<T>>) value.get();
+  }
+
+  @SuppressWarnings("unchecked")
+  static <T> Endpoint<Map<String, T>> forMap(String endpoint, Class<T> clazz) {
+    WeakReference<Endpoint<?>> value = ENDPOINTS.get(endpoint);
+    if (value == null || value.get() == null) {
+      value =
+          new WeakReference<>(new Endpoint<>(endpoint, JSON_OBJECT_MAPPER.readerForMapOf(clazz)));
+      ENDPOINTS.put(endpoint, value);
+    }
+    return (Endpoint<Map<String, T>>) value.get();
+  }
+
+  public static void setExecutor(ExecutorService executor) {
+    Objects.requireNonNull(executor);
+    EXECUTOR.shutdown();
+    EXECUTOR = executor;
+  }
+
+  public static void shutdown() {
+    EXECUTOR.shutdown();
+  }
+}

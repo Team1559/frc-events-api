a63e52ed6d38eedb9020bc6dce229f159d053e95
MathNerd28 <xander.bhalla@gmail.com>
2024-03-18 11:34:28 -0400

    Implement Alliances (/:season/alliances/:eventCode)

---
+++ frc-events-api-java/src/main/java/org/victorrobotics/frcevents/Alliances.java
@@ -0,0 +1,22 @@
+package org.victorrobotics.frcevents;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Alliances(@JsonProperty("Alliances") List<Alliance> alliances,
+                        @JsonProperty int count) {
+  public record Alliance(@JsonProperty int number,
+                         @JsonProperty String name,
+                         @JsonProperty int captain,
+                         @JsonProperty int round1,
+                         @JsonProperty int round2,
+                         @JsonProperty Integer round3,
+                         /* Unable to determine format */
+                         @JsonProperty Object backup,
+                         @JsonProperty Object backupReplaced) {}
+
+  public static Endpoint<Alliances> forEvent(int year, String eventCode) {
+    return Endpoint.forSingle("/" + year + "/alliances/" + eventCode, Alliances.class);
+  }
+}

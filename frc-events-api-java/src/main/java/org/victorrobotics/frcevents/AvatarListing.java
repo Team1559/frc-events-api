680c3ab68463e09cb8c89b9f4fd9e705da0d5b97
MathNerd28 <xander.bhalla@gmail.com>
2024-03-17 22:51:15 -0400

    Implement AvatarListing (/:season/avatars)

---
+++ frc-events-api-java/src/main/java/org/victorrobotics/frcevents/AvatarListing.java
@@ -0,0 +1,37 @@
+package org.victorrobotics.frcevents;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record AvatarListing(@JsonProperty List<TeamAvatar> teams,
+                            @JsonProperty int teamCountTotal,
+                            @JsonProperty int teamCountPage) {
+  public record TeamAvatar(@JsonProperty int teamNumber,
+                           @JsonProperty String encodedAvatar) {}
+
+  public static Endpoint<AvatarListing> forTeam(int year, int teamNumber) {
+    return Endpoint.forSingle("/" + year + "/avatars?teamNumber=" + teamNumber,
+                              AvatarListing.class);
+  }
+
+  public static Endpoint<AvatarListing> forAll(int year, int pageNumber) {
+    return forQuery(year, new Query().withPage(pageNumber));
+  }
+
+  public static Endpoint<AvatarListing> forQuery(int year, Query query) {
+    return Endpoint.forSingle("/" + year + "/avatars" + query.build(), AvatarListing.class);
+  }
+
+  public static class Query extends QueryBuilder {
+    public Query withEvent(String eventCode) {
+      putParam("eventCode", eventCode);
+      return this;
+    }
+
+    public Query withPage(int pageNumber) {
+      putParam("page", pageNumber);
+      return this;
+    }
+  }
+}
